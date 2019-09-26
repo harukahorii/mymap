26fc330970f5f458a6121b4ae4e2bd1b194d8163
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-09-26 10:12:48 +0900

    Create MyMap.vue

---
+++ app/javascript/MyMap.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Map</h1>
+    <map-loader
+      :map-config="mapConfig"
+      apiKey=Rails.application.credentials.google_maps_api_key>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
